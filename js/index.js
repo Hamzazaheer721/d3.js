68c59f2ec58954875071d251fc8f48aba45bb99f
Hamza Zaheer <hamzazaheer721@gmail.com>
2023-12-16 17:39:25 +0500

    feature-d3-setup: add data loading and binding

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 const SECTIONS = {
   SELECTION_AND_MANIPULATION: "SELECTION_AND_MANIPULATION",
-  DATA_LOADING_AND_BINDING: "DATA_LOADING_AND_BINDING"
+  DATA_LOADING_AND_BINDING: "DATA_LOADING_AND_BINDING",
+  BAR_CHART: "BAR_CHART"
 }
+
+const body = d3.select("body")
 
 const selectionAndManipulation = () => {
   const heading = d3
@@ -11,7 +14,6 @@ const selectionAndManipulation = () => {
     .text("This is updated by d3")
 
   /* dynamically adding paragraphs */
-  const body = d3.select("body")
 
   body.append("p").text("First Paragraph")
   body.append("p").text("Second Paragraph")
@@ -20,10 +22,26 @@ const selectionAndManipulation = () => {
   d3.selectAll("p").style("color", "blue")
 }
 
-const dataLoadingAndBinding = () => {}
+const dataLoadingAndBinding = () => {
+  const data = [
+    "First Paragraph",
+    "Second Paragraph",
+    "Third Paragraph",
+    "Fourth Paragraph",
+    "Fifth Paragraph"
+  ]
+
+  const paragraphs = body.selectAll("p")
+
+  paragraphs
+    .data(data)
+    .enter()
+    .append("p")
+    .text((d) => d)
+}
 
 function main() {
-  const section = SECTIONS.SELECTION_AND_MANIPULATION
+  const section = SECTIONS.DATA_LOADING_AND_BINDING
   switch (section) {
     case SECTIONS.SELECTION_AND_MANIPULATION:
       selectionAndManipulation()
